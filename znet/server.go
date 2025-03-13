eb72c80b4294a3a7ce16037bd94c04f5b6af3bee
jyh-centos9 <568000273@qq.com>
2025-03-13 15:59:15 +0800

    add basic router feature.

--- znet/server.go
+++ znet/server.go
@@ -1,6 +1,7 @@
 package znet
 
 import (
+	"errors"
 	"fmt"
 	"net"
 	"time"
@@ -18,6 +19,19 @@ type Server struct {
 	IP string
 	//服务绑定的端口
 	Port int
+	//当前Server由用户绑定的回调router,也就是Server注册的链接对应的处理业务
+	Router ziface.IRouter
+}
+
+// ============== 定义当前客户端链接的handle api ===========
+func CallBackToClient(conn *net.TCPConn, data []byte, cnt int) error {
+	//回显业务
+	fmt.Println("[Conn Handle] CallBackToClient ... ")
+	if _, err := conn.Write(data[:cnt]); err != nil {
+		fmt.Println("write back buf err ", err)
+		return errors.New("CallBackToClient error")
+	}
+	return nil
 }
 
 //============== 实现 ziface.IServer 里的全部接口方法 ========
@@ -45,6 +59,9 @@ func (s *Server) Start() {
 		//已经监听成功
 		fmt.Println("start Zinx server  ", s.Name, " succ, now listenning...")
 
+		var cid uint32
+		cid = 0
+
 		//3 启动server网络连接业务
 		for {
 			//3.1 阻塞等待客户端建立连接请求
@@ -56,25 +73,12 @@ func (s *Server) Start() {
 
 			//3.2 TODO Server.Start() 设置服务器最大连接控制,如果超过最大连接，那么则关闭此新的连接
 
-			//3.3 TODO Server.Start() 处理该新连接请求的 业务 方法， 此时应该有 handler 和 conn是绑定的
+			//3.3 处理该新连接请求的 业务 方法， 此时应该有 handler 和 conn是绑定的
+			dealConn := NewConntion(conn, cid, s.Router)
+			cid++
 
-			//我们这里暂时做一个最大512字节的回显服务
-			go func() {
-				//不断的循环从客户端获取数据
-				for {
-					buf := make([]byte, 512)
-					cnt, err := conn.Read(buf)
-					if err != nil {
-						fmt.Println("recv buf err ", err)
-						continue
-					}
-					//回显
-					if _, err := conn.Write(buf[:cnt]); err != nil {
-						fmt.Println("write back buf err ", err)
-						continue
-					}
-				}
-			}()
+			//3.4 启动当前链接的处理业务
+			go dealConn.Start()
 		}
 	}()
 }
@@ -96,6 +100,13 @@ func (s *Server) Serve() {
 	}
 }
 
+// 路由功能：给当前服务注册一个路由业务方法，供客户端链接处理使用
+func (s *Server) AddRouter(router ziface.IRouter) {
+	s.Router = router
+
+	fmt.Println("Add Router succ! ")
+}
+
 /*
 创建一个服务器句柄
 */
@@ -105,6 +116,7 @@ func NewServer(name string) ziface.IServer {
 		IPVersion: "tcp4",
 		IP:        "0.0.0.0",
 		Port:      7777,
+		Router:    nil,
 	}
 
 	return s
--- znet/server_test.go
+++ znet/server_test.go
@@ -5,7 +5,41 @@ import (
 	"net"
 	"testing"
 	"time"
+
+	"github.com/KinoHui/project-zinx/ziface"
 )
+
+// ping test 自定义路由
+type PingRouter struct {
+	BaseRouter //一定要先基础BaseRouter
+}
+
+// Test PreHandle
+func (this *PingRouter) PreHandle(request ziface.IRequest) {
+	fmt.Println("Call Router PreHandle")
+	_, err := request.GetConnection().GetTCPConnection().Write([]byte("before ping ....\n"))
+	if err != nil {
+		fmt.Println("call back ping ping ping error")
+	}
+}
+
+// Test Handle
+func (this *PingRouter) Handle(request ziface.IRequest) {
+	fmt.Println("Call PingRouter Handle")
+	_, err := request.GetConnection().GetTCPConnection().Write([]byte("ping...ping...ping\n"))
+	if err != nil {
+		fmt.Println("call back ping ping ping error")
+	}
+}
+
+// Test PostHandle
+func (this *PingRouter) PostHandle(request ziface.IRequest) {
+	fmt.Println("Call Router PostHandle")
+	_, err := request.GetConnection().GetTCPConnection().Write([]byte("After ping .....\n"))
+	if err != nil {
+		fmt.Println("call back ping ping ping error")
+	}
+}
 
 /*
 模拟客户端
@@ -23,7 +57,7 @@ func ClientTest() {
 	}
 
 	for {
-		_, err := conn.Write([]byte("hello ZINX"))
+		_, err := conn.Write([]byte("Zinx V0.3"))
 		if err != nil {
 			fmt.Println("write error err ", err)
 			return
@@ -49,7 +83,9 @@ func TestServer(t *testing.T) {
 		服务端测试
 	*/
 	//1 创建一个server 句柄 s
-	s := NewServer("[zinx V0.1]")
+	s := NewServer("[zinx V0.3]")
+
+	s.AddRouter(&PingRouter{})
 
 	/*
 		客户端测试
