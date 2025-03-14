8987c21c5ec96704f325084072e5caddcbd710f5
jyh-centos9 <568000273@qq.com>
2025-03-14 15:31:18 +0800

    feat: add multi-route mode

--- znet/server_test.go
+++ znet/server_test.go
@@ -12,17 +12,32 @@ import (
 
 // ping test 自定义路由
 type PingRouter struct {
-	BaseRouter //一定要先基础BaseRouter
+	BaseRouter
 }
 
-// Test Handle
+// Ping Handle
 func (this *PingRouter) Handle(request ziface.IRequest) {
 	fmt.Println("Call PingRouter Handle")
 	//先读取客户端的数据，再回写ping...ping...ping
 	fmt.Println("recv from client : msgId=", request.GetMsgID(), ", data=", string(request.GetData()))
 
-	//回写数据
-	err := request.GetConnection().SendMsg(1, []byte("ping...ping...ping"))
+	err := request.GetConnection().SendMsg(0, []byte("ping...ping...ping"))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+// HelloZinxRouter Handle
+type HelloZinxRouter struct {
+	BaseRouter
+}
+
+func (this *HelloZinxRouter) Handle(request ziface.IRequest) {
+	fmt.Println("Call HelloZinxRouter Handle")
+	//先读取客户端的数据，再回写ping...ping...ping
+	fmt.Println("recv from client : msgId=", request.GetMsgID(), ", data=", string(request.GetData()))
+
+	err := request.GetConnection().SendMsg(1, []byte("Hello Zinx Router V0.6"))
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -31,7 +46,7 @@ func (this *PingRouter) Handle(request ziface.IRequest) {
 /*
 模拟客户端
 */
-func ClientTest() {
+func Client0Test() {
 
 	fmt.Println("Client Test ... start")
 	//3秒之后发起测试请求，给服务端开启服务的机会
@@ -86,6 +101,61 @@ func ClientTest() {
 	}
 }
 
+func Client1Test() {
+
+	fmt.Println("Client Test ... start")
+	//3秒之后发起测试请求，给服务端开启服务的机会
+	time.Sleep(3 * time.Second)
+
+	conn, err := net.Dial("tcp", "127.0.0.1:7777")
+	if err != nil {
+		fmt.Println("client start err, exit!")
+		return
+	}
+
+	for {
+		//发封包message消息
+		dp := NewDataPack()
+		msg, _ := dp.Pack(NewMsgPackage(1, []byte("Zinx V0.5 Client Test Message")))
+		_, err := conn.Write(msg)
+		if err != nil {
+			fmt.Println("write error err ", err)
+			return
+		}
+
+		//先读出流中的head部分
+		headData := make([]byte, dp.GetHeadLen())
+		_, err = io.ReadFull(conn, headData) //ReadFull 会把msg填充满为止
+		if err != nil {
+			fmt.Println("read head error")
+			break
+		}
+		//将headData字节流 拆包到msg中
+		msgHead, err := dp.Unpack(headData)
+		if err != nil {
+			fmt.Println("server unpack err:", err)
+			return
+		}
+
+		if msgHead.GetDataLen() > 0 {
+			//msg 是有data数据的，需要再次读取data数据
+			msg := msgHead.(*Message)
+			msg.Data = make([]byte, msg.GetDataLen())
+
+			//根据dataLen从io中读取字节流
+			_, err := io.ReadFull(conn, msg.Data)
+			if err != nil {
+				fmt.Println("server unpack data err:", err)
+				return
+			}
+
+			fmt.Println("==> Recv Msg: ID=", msg.Id, ", len=", msg.DataLen, ", data=", string(msg.Data))
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+}
+
 // Server 模块的测试函数
 func TestServer(t *testing.T) {
 
@@ -95,12 +165,14 @@ func TestServer(t *testing.T) {
 	//1 创建一个server 句柄 s
 	s := NewServer("[zinx V0.3]")
 
-	s.AddRouter(&PingRouter{})
+	s.AddRouter(0, &PingRouter{})
+	s.AddRouter(1, &HelloZinxRouter{})
 
 	/*
 		客户端测试
 	*/
-	go ClientTest()
+	go Client0Test()
+	go Client1Test()
 
 	//2 开启服务
 	s.Serve()
